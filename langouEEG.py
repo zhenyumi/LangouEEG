09222fa2b422c4fc6831722bb7bd107d03e2617b
zhenyumi <47143032+zhenyumi@users.noreply.github.com>
2021-06-22 19:11:09 +0800

    Version 0.5

--- langouEEG.py
+++ langouEEG.py
@@ -9,27 +9,28 @@ from copy import deepcopy
 from mne.preprocessing import create_ecg_epochs, create_eog_epochs, read_ica
 import sys
 #mne.utils.set_config('MNE_USE_CUDA', 'true')  
-def init_prog():
-    global ratio_TD_all_r
-    ratio_TD_all_r = []
-    global ratio_TU_all_r
-    ratio_TU_all_r = []
-    global ratio_DU_all_r
-    ratio_DU_all_r = []
-    global ratio_TD_all_f
-    ratio_TD_all_f = []
-    global ratio_TU_all_f
-    ratio_TU_all_f = []
-    global ratio_DU_all_f
-    ratio_DU_all_f = []
+def init_prog(MA_n=20.0):
+    global n_of_MA
+    n_of_MA = MA_n
+    global ratio_TD_all_r, ratio_TU_all_r, ratio_DU_all_r
+    ratio_TD_all_r, ratio_TU_all_r, ratio_DU_all_r = [],[],[]
+
+    global ratio_TD_all_f, ratio_TU_all_f, ratio_DU_all_f
+    ratio_TD_all_f, ratio_TU_all_f, ratio_DU_all_f = [],[],[]
+    
+    global ratioMA_TD_all_r, ratioMA_TU_all_r, ratioMA_DU_all_r
+    ratioMA_TD_all_r, ratioMA_TU_all_r, ratioMA_DU_all_r = [],[],[]
+    
+    global ratioMA_TD_all_f, ratioMA_TU_all_f, ratioMA_DU_all_f
+    ratioMA_TD_all_f, ratioMA_TU_all_f, ratioMA_DU_all_f = [],[],[]
+    
     return
 def save_ratios(filefolder = sys.path[0] + '\\Light'):
-    global ratio_TD_all_r
-    global ratio_TU_all_r
-    global ratio_DU_all_r
-    global ratio_TD_all_f
-    global ratio_TU_all_f
-    global ratio_DU_all_f
+    global ratio_TD_all_r, ratio_TU_all_r, ratio_DU_all_r
+    global ratio_TD_all_f, ratio_TU_all_f, ratio_DU_all_f
+    global ratioMA_TD_all_r, ratioMA_TU_all_r, ratioMA_DU_all_r
+    global ratioMA_TD_all_f, ratioMA_TU_all_f, ratioMA_DU_all_f
+    
     df_r = pd.DataFrame()
     df_r['ratio_TD'] = ratio_TD_all_r
     df_r['ratio_TU'] = ratio_TU_all_r
@@ -40,6 +41,17 @@ def save_ratios(filefolder = sys.path[0] + '\\Light'):
     df_f['ratio_TU'] = ratio_TU_all_f
     df_f['ratio_DU'] = ratio_DU_all_f
     df_f.to_csv(filefolder + '\\ratios_flicker.csv')
+    dfMA_r = pd.DataFrame()
+    dfMA_r['ratio_TD'] = ratioMA_TD_all_r
+    dfMA_r['ratio_TU'] = ratioMA_TU_all_r
+    dfMA_r['ratio_DU'] = ratioMA_DU_all_r
+    dfMA_r.to_csv(filefolder + '\\ratiosMA_rest.csv')
+    dfMA_f = pd.DataFrame()
+    dfMA_f['ratio_TD'] = ratioMA_TD_all_f
+    dfMA_f['ratio_TU'] = ratioMA_TU_all_f
+    dfMA_f['ratio_DU'] = ratioMA_DU_all_f
+    dfMA_f.to_csv(filefolder + '\\ratiosMA_flicker.csv')
+    return
 def csv_transformat(filefolder = sys.path[0] + '\\Light', type='flicker'):
     filefolder = sys.path[0] + '\\Light'
     df=pd.read_csv(filefolder + '\\ratios_{0}.csv'.format(type))
@@ -60,6 +72,28 @@ def csv_transformat(filefolder = sys.path[0] + '\\Light', type='flicker'):
     save['ratios'] = ratios
     save['labels'] = labels
     save.to_csv(filefolder + '\\ratios_{0}_all.csv'.format(type))
+    print('{0} CSV transformation complete'.format(type))
+    return
+def csv_transformat_MA(filefolder = sys.path[0] + '\\Light', type='flicker'):
+    filefolder = sys.path[0] + '\\Light'
+    df=pd.read_csv(filefolder + '\\ratiosMA_{0}.csv'.format(type))
+    shape = df.shape
+    print(shape)
+    rows = shape[0]
+    column = shape[1]
+    col_names = df.columns.values.tolist()
+    ratios = []
+    labels = []
+    print(col_names)
+    for i in range(1, column):
+        ratio = df.iloc[0:rows,i].tolist()
+        type_label = [col_names[i]] * len(ratio)
+        ratios += ratio
+        labels += type_label
+    save = pd.DataFrame()
+    save['ratios'] = ratios
+    save['labels'] = labels
+    save.to_csv(filefolder + '\\ratiosMA_{0}_all.csv'.format(type))
     print('{0} CSV transformation complete'.format(type))
     return
 def initData(subject_name,picks_str=['O1','O2','OZ']):
@@ -176,7 +210,8 @@ def extractEpochs(raw,events,picks,tmin_rest = 60,tmax_rest = 120,tmin_flick = 3
     #evoked_4F.plot(time_unit='s')
     return epoch_RR,epoch_RF,epoch_4R,epoch_4F
 def doMA(psds):
-    n=20.0
+    global n_of_MA
+    n = n_of_MA
     for i in range(psds.shape[0]):
         tempSum=0
         for j in range(int(n)):
@@ -186,6 +221,11 @@ def doMA(psds):
                 n=j
                 break
         psds[i]=tempSum/n
+    return psds
+def doMA3D(psds):
+    for i in range(psds.shape[0]):
+        for j in range(psds.shape[1]):
+            psds[i][j] = doMA(psds[i][j])
     return psds
 def add_arrows(axes):
     # add some arrows at 60 Hz and its harmonics
@@ -206,9 +246,9 @@ def plot_psd_sub(epoch,ax, fmin=.1, fmax=100, n_jobs=8, color='k', alpha=.5, lab
     psds, freqs = psd_multitaper(epoch, fmin=fmin, fmax=fmax, n_jobs=n_jobs)
     # MA: 是否做滑动平均
     if isma:
-        psds = doMA(psds)
+        psds = doMA3D(psds)
     psds = 10. * np.log10(psds)
-    psds_mean = doMA(psds.mean(0).mean(0))
+    psds_mean = psds.mean(0).mean(0)
     psds_std = psds.mean(0).std(0)
     ax.plot(freqs, psds_mean, color=color, label = label)
     ax.fill_between(freqs, psds_mean - psds_std, psds_mean + psds_std,
@@ -262,11 +302,8 @@ def superpos85(epoch_4R,epoch_4F,epoch_RF,subject_name,MA=False):
     plt.xlabel("Frequency")
     plt.ylabel("Power spectral density (PSD) in log")
     plt.savefig(sys.path[0] + '\\Light\\Light_figures_non\\' + subject_name + '_40_85.png')
-def getRatio_rest(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2'], MA=False):
+def getRatio_rest(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2']):
     psds, freqs = psd_multitaper(epoch,fmin=fmin, fmax=fmax, n_jobs=8,picks=picks) 
-    # MA: 是否做滑动平均
-    if MA:
-        psds = doMA(psds)
     # psd.shape: (number of epoch, number of channel, frequency)
     f_down = 35.0
     f_low = 39.0
@@ -326,11 +363,8 @@ def getRatio_rest(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2'], MA=False):
     print(r'The downstream/upstream is:')
     print(ratio_DU)
     return downstream_mean_power,target_mean_power,upstream_mean_power
-def getRatio_flicker(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2'], MA = False):
+def getRatio_flicker(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2']):
     psds, freqs = psd_multitaper(epoch,fmin=fmin, fmax=fmax, n_jobs=8,picks=picks) 
-    # MA: 是否做滑动平均
-    if MA:
-        psds = doMA(psds)
     # psd.shape: (number of epoch, number of channel, frequency)
     f_down = 35.0
     f_low = 39.0
@@ -382,6 +416,146 @@ def getRatio_flicker(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2'], MA = Fa
         ratio_DU.append(DU)
         global ratio_DU_all_f
         ratio_DU_all_f.append(DU)
+    print(r'The target/downstream is:')
+    print(ratio_TD)
+    print(r'The target/upstream is:')
+    print(ratio_TU)
+    print(r'The downstream/upstream is:')
+    print(ratio_DU)
+    return downstream_mean_power,target_mean_power,upstream_mean_power
+def get_minima(data):
+    center = np.argmax(data)
+    print(center)
+    min_right = data[center]
+    min_left = data[center]
+    for i in range(center, data.shape[0]):
+        if data[i+1] <= min_right:
+            min_right = data[i+1]
+        else:
+            break
+    for i in range(center, 0, -1):
+        if data[i-1] <= min_left:
+            min_left = data[i-1]
+        else:
+            break
+    return min_left, min_right
+def getRatio_flicker_MA(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2']):
+    psds, freqs = psd_multitaper(epoch,fmin=fmin, fmax=fmax, n_jobs=8,picks=picks) 
+    # MA: 做滑动平均
+    psds = doMA3D(psds)
+    # psd.shape: (number of epoch, number of channel, frequency)
+    f_down = 35.0
+    f_low = 39.0
+    f_high = 41.0
+    f_upstream = 45.0
+    print(psds.shape)
+    num_of_epoch = psds.shape[0]
+    num_of_channel = psds[0].shape[0]
+    print("{0} epochs in total".format(num_of_epoch))
+    print("{0} channels in total".format(num_of_channel))
+    print(psds.shape)
+    # average all channels
+    psds = np.mean(psds, axis=1)
+    # extract power in selected frequency bands
+    downstream_mean_power, target_mean_power, upstream_mean_power = [],[],[]
+    for i in range(0, num_of_epoch):
+        a,b,c = [],[],[]
+        for j in range(0, len(freqs)):
+            freq = freqs[j]
+            power = psds[i][j]
+            if freq < f_low and freq > f_down:
+                a.append(power)
+            if freq < f_high and freq > f_low:
+                b.append(power)
+            if freq < f_upstream and freq > f_high:
+                c.append(power)
+        downstream_mean_power.append(np.min(a))
+        target_mean_power.append(np.max(b))
+        upstream_mean_power.append(np.min(c))
+    print(r'The downstream min power is:')
+    print(downstream_mean_power)
+    print(r'The taget band max power is:')
+    print(target_mean_power)
+    print(r'The upstream min power is:')
+    print(upstream_mean_power)
+    ratio_TD = []
+    ratio_TU = []
+    ratio_DU = []
+    for i in range(0, len(target_mean_power)):
+        TD = target_mean_power[i]/downstream_mean_power[i]
+        TU = target_mean_power[i]/upstream_mean_power[i]
+        DU = downstream_mean_power[i]/upstream_mean_power[i]
+        ratio_TD.append(TD)
+        global ratioMA_TD_all_f
+        ratioMA_TD_all_f.append(TD)
+        ratio_TU.append(TU)
+        global ratioMA_TU_all_f
+        ratioMA_TU_all_f.append(TU)
+        ratio_DU.append(DU)
+        global ratioMA_DU_all_f
+        ratioMA_DU_all_f.append(DU)
+    print(r'The target/downstream is:')
+    print(ratio_TD)
+    print(r'The target/upstream is:')
+    print(ratio_TU)
+    print(r'The downstream/upstream is:')
+    print(ratio_DU)
+    return downstream_mean_power,target_mean_power,upstream_mean_power
+def getRatio_rest_MA(epoch,fmin=35.0,fmax=45.0,picks=['O1', 'OZ', 'O2']):
+    psds, freqs = psd_multitaper(epoch,fmin=fmin, fmax=fmax, n_jobs=8,picks=picks) 
+    # MA: 做滑动平均
+    psds = doMA3D(psds)
+    # psd.shape: (number of epoch, number of channel, frequency)
+    f_down = 35.0
+    f_low = 39.0
+    f_high = 41.0
+    f_upstream = 45.0
+    print(psds.shape)
+    num_of_epoch = psds.shape[0]
+    num_of_channel = psds[0].shape[0]
+    print("{0} epochs in total".format(num_of_epoch))
+    print("{0} channels in total".format(num_of_channel))
+    print(psds.shape)
+    # average all channels
+    psds = np.mean(psds, axis=1)
+    # extract power in selected frequency bands
+    downstream_mean_power, target_mean_power, upstream_mean_power = [],[],[]
+    for i in range(0, num_of_epoch):
+        a,b,c = [],[],[]
+        for j in range(0, len(freqs)):
+            freq = freqs[j]
+            power = psds[i][j]
+            if freq < f_low and freq > f_down:
+                a.append(power)
+            if freq < f_high and freq > f_low:
+                b.append(power)
+            if freq < f_upstream and freq > f_high:
+                c.append(power)
+        downstream_mean_power.append(np.min(a))
+        target_mean_power.append(np.max(b))
+        upstream_mean_power.append(np.min(c))
+    print(r'The downstream min power is:')
+    print(downstream_mean_power)
+    print(r'The taget band max power is:')
+    print(target_mean_power)
+    print(r'The upstream min power is:')
+    print(upstream_mean_power)
+    ratio_TD = []
+    ratio_TU = []
+    ratio_DU = []
+    for i in range(0, len(target_mean_power)):
+        TD = target_mean_power[i]/downstream_mean_power[i]
+        TU = target_mean_power[i]/upstream_mean_power[i]
+        DU = downstream_mean_power[i]/upstream_mean_power[i]
+        ratio_TD.append(TD)
+        global ratioMA_TD_all_r
+        ratioMA_TD_all_r.append(TD)
+        ratio_TU.append(TU)
+        global ratioMA_TU_all_r
+        ratioMA_TU_all_r.append(TU)
+        ratio_DU.append(DU)
+        global ratioMA_DU_all_r
+        ratioMA_DU_all_r.append(DU)
     print(r'The target/downstream is:')
     print(ratio_TD)
     print(r'The target/upstream is:')
